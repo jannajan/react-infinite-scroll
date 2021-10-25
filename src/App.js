7a61e9916dbf9c6c4a8802cd81eb8babf3ee7a09
jannajan <janj58102@gmail.com>
2021-10-24 19:04:17 -0500

    Plug in state and hook and display everything for now

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
+import React, { useState } from 'react'
+import useBookSearch from './hooks/useBookSearch'
 import './App.css'
 
 function App() {
+  const [query, setQuery] = useState('')
+  const [pageNumber, setPageNumber] = useState(1)
+
+  const handleSearch = (e) => {
+    setQuery(e.target.value)
+    setPageNumber(1)
+  }
+
+  const {
+    isLoading,
+    hasError,
+    books,
+    hasMore
+  } = useBookSearch(query, pageNumber)
+
   return (
     <div className="App">
       <header className="App-header">
         <div>Infinite Scroll Playground</div>
       </header>
+      <>
+        <input
+          type="text"
+          onChange={handleSearch}
+        />
+        {books.map((book, idx) => {
+          return <div key={idx}>{book}</div>
+        })}
+        <div>{isLoading ? 'Loading search results...' : null}</div>
+        <div>{hasError ? 'Oops, something went wrong' : null}</div>
+      </>
     </div>
   );
 }
